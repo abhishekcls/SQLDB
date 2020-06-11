102a57dcd28ebfd2027af82974bb183bdf4f25eb
abhishekcls <44670121+abhishekcls@users.noreply.github.com>
2020-06-11 15:20:22 +0530

    Add files via upload

--- Bulk Insert.sql
+++ Bulk Insert.sql
@@ -1,7 +1,36 @@
-BULK INSERT Finance.dbo.Currency
-FROM 'D:\Demofiles\Mod06\Currency.csv'
+CREATE TABLE [Currency](
+	[CurrencyCode] [nchar](3) NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](50) NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_Currency_ModifiedDate] DEFAULT (getdate()) 
+	);
+GO
+
+BULK INSERT Currency
+FROM 'c:\data\Currency.csv'
 WITH 
       (
          FIELDTERMINATOR =',',
          ROWTERMINATOR ='\n'
       );
+
+
+select * from currency
+
+--Another Example
+
+CREATE TABLE ExchangeRate
+( ExchangeRateID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  CurrencyCode char(3) NOT NULL,
+  ExchangeRate decimal(18, 5) NOT NULL,
+  ConversionDate datetime NOT NULL,
+);
+
+BULK INSERT ExchangeRate
+FROM 'C:\Program Files\Microsoft Learning\20462\Allfiles\Demofiles\Mod06\ExchangeRates.csv' 
+WITH ( FORMATFILE='C:\Program Files\Microsoft Learning\20462\Allfiles\Demofiles\Mod06\ExchangeRates.xml',
+       BATCHSIZE=10000,
+       FIRSTROW=2
+     );
+GO
+
+select * from ExchangeRate
--- OpenRowset.sql
+++ OpenRowset.sql
@@ -1,3 +1,17 @@
-INSERT INTO Finance.dbo.SalestaxRate
-SELECT * FROM OPENROWSET (BULK 'D:\Demofiles\Mod06\SalesTaxRate.csv’,
-FORMATFILE = 'D:\Demofiles\Mod06\TaxRateFmt.xml') AS rows;
+CREATE TABLE [SalesTaxRate](
+	[SalesTaxRateID] [int] NOT NULL PRIMARY KEY,
+	[StateProvinceID] [int] NOT NULL,
+	[TaxType] [tinyint] NOT NULL,
+	[TaxRate] [smallmoney] NOT NULL CONSTRAINT [DF_SalesTaxRate_TaxRate]  DEFAULT ((0.00)),
+	[Name] [nvarchar](50) NOT NULL,
+	[rowguid] [uniqueidentifier] NOT NULL CONSTRAINT [DF_SalesTaxRate_rowguid]  DEFAULT (newid()),
+	[ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_SalesTaxRate_ModifiedDate]  DEFAULT (getdate()),
+ );
+GO
+
+INSERT INTO SalestaxRate
+SELECT * FROM OPENROWSET (BULK 'C:\Program Files\Microsoft Learning\20462\Allfiles\Demofiles\Mod06\SalesTaxRate.csv',
+FORMATFILE = 'C:\Program Files\Microsoft Learning\20462\Allfiles\Demofiles\Mod06\TaxRateFmt.xml') AS rows;
+
+
+select * from [SalesTaxRate]
